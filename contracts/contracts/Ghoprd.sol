c47f3ce391f0cb3f2a0ffb507b3f63042a182766
Leontiy Yashkov-Kopystynskiy <72278156+luckerninja@users.noreply.github.com>
2024-01-16 22:51:03 +0300

    rename project

--- contracts/contracts/Ghoprd.sol
+++ contracts/contracts/Ghoprd.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-contract Ghobet {
+contract Ghoprd {
     struct Bet {
         address bettor;
         uint amount;
