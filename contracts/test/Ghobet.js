9b5e1be78629c3870ee6902f8a9cb651d46fb4bf
Leontiy Yashkov-Kopystynskiy <72278156+luckerninja@users.noreply.github.com>
2024-01-16 13:33:48 +0300

    base contract tests

--- contracts/test/Ghobet.js
+++ contracts/test/Ghobet.js
@@ -1,71 +1,37 @@
-const { time, loadFixture } = require("@nomicfoundation/hardhat-toolbox/network-helpers");
-const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
 const { expect } = require("chai");
 
 describe("Ghobet", function () {
-  async function deployGhoBetFixture() {
-    const [owner, moderator, user1, user2] = await ethers.getSigners();
-    const GhoToken = await ethers.getContractFactory("GhoToken"); // Make sure you have GhoToken contract
-    const ghoToken = await GhoToken.deploy();
-    const GhoBet = await ethers.getContractFactory("GhoBet");
-    const ghoBet = await GhoBet.deploy(ghoToken.address);
-    await ghoToken.transfer(user1.address, ethers.utils.parseEther("100"));
-    await ghoToken.transfer(user2.address, ethers.utils.parseEther("100"));
-    return { ghoBet, ghoToken, owner, moderator, user1, user2 };
-  }
+  let ghobet;
+  let ghoToken;
 
-  describe("Deployment", function () {
-    it("Should set the right GhoToken address", async function () {
-      const { ghoBet, ghoToken } = await loadFixture(deployGhoBetFixture);
-      expect(await ghoBet.ghoToken()).to.equal(ghoToken.address);
-    });
+  before(async () => {
+    // Замените "GhoTokenAddress" на реальный адрес вашего токена GHO
+    const GhoTokenAddress = "0x..."; 
 
-    it("Should set the deployer as a moderator", async function () {
-      const { ghoBet, owner } = await loadFixture(deployGhoBetFixture);
-      expect(await ghoBet.moderators(owner.address)).to.equal(true);
-    });
+    const GhoToken = await ethers.getContractFactory("GhoToken");
+    ghoToken = await GhoToken.deploy();
+
+    const Ghobet = await ethers.getContractFactory("Ghobet");
+    ghobet = await Ghobet.deploy(GhoTokenAddress);
   });
 
-  describe("Prediction Management", function () {
-    it("Should allow users to make predictions", async function () {
-      const { ghoBet, user1 } = await loadFixture(deployGhoBetFixture);
-      await expect(ghoBet.connect(user1).makePrediction("Will it rain tomorrow?", time.latest() + 60)).to.emit(ghoBet, "PredictionMade");
-    });
+  it("should create a prediction and retrieve it through view function", async function () {
+    const predictionText = "Will it rain tomorrow?";
+    const endDate = Math.floor(Date.now() / 1000) + 86400; // 1 day from now
 
-    it("Should allow users to get predictions", async function () {
-      const { ghoBet, user1 } = await loadFixture(deployGhoBetFixture);
-      await ghoBet.connect(user1).makePrediction("Will it rain tomorrow?", time.latest() + 60);
-      const prediction = await ghoBet.getPredictions(0);
-      expect(prediction.text).to.equal("Will it rain tomorrow?");
-    });
-  });
+    // Создаем предсказание
+    await ghobet.makePrediction(predictionText, endDate);
 
-  describe("Betting", function () {
-    it("Should allow users to place bets", async function () {
-      const { ghoBet, user1 } = await loadFixture(deployGhoBetFixture);
-      await ghoBet.connect(user1).makePrediction("Will it rain tomorrow?", time.latest() + 60);
-      await expect(ghoBet.connect(user1).betOnPrediction(0, ethers.utils.parseEther("10"), true)).to.emit(ghoBet, "BetPlaced");
-    });
-  });
+    // Получаем созданное предсказание через view функцию
+    const predictions = await ghobet.getPredictions();
+    const retrievedPrediction = await ghobet.getPrediction(predictions.length - 1);
 
-  describe("Prediction Resolution", function () {
-    it("Should allow moderators to resolve predictions", async function () {
-      const { ghoBet, moderator } = await loadFixture(deployGhoBetFixture);
-      await ghoBet.connect(moderator).makePrediction("Will it rain tomorrow?", time.latest() + 60);
-      await ghoBet.connect(moderator).betOnPrediction(0, ethers.utils.parseEther("10"), true);
-      await time.increaseTo(time.latest() + 61);
-      await expect(ghoBet.connect(moderator).resolvePrediction(0, true)).to.emit(ghoBet, "PredictionResolved");
-    });
-  });
-
-  describe("Claiming Winnings", function () {
-    it("Should allow users to claim winnings", async function () {
-      const { ghoBet, moderator, user1 } = await loadFixture(deployGhoBetFixture);
-      await ghoBet.connect(moderator).makePrediction("Will it rain tomorrow?", time.latest() + 60);
-      await ghoBet.connect(moderator).betOnPrediction(0, ethers.utils.parseEther("10"), true);
-      await time.increaseTo(time.latest() + 61);
-      await ghoBet.connect(moderator).resolvePrediction(0, true);
-      await expect(ghoBet.connect(user1).claimWinnings(0)).to.not.be.reverted;
-    });
+    // Проверяем, что созданное и полученное предсказание идентичны
+    expect(retrievedPrediction.text).to.equal(predictionText);
+    expect(retrievedPrediction.endDate).to.equal(endDate);
+    expect(retrievedPrediction.totalFor).to.equal(0);
+    expect(retrievedPrediction.totalAgainst).to.equal(0);
+    expect(retrievedPrediction.resolved).to.equal(false);
+    expect(retrievedPrediction.result).to.equal(false);
   });
 });
