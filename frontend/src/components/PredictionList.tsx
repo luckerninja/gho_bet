3fc952d772ae8205382cb3383c7b559d0b125de5
Leontiy Yashkov-Kopystynskiy <72278156+luckerninja@users.noreply.github.com>
2024-01-17 19:25:50 +0300

    mini prediction list fixes

--- frontend/src/components/PredictionList.tsx
+++ frontend/src/components/PredictionList.tsx
@@ -34,13 +34,13 @@ export default function PredictionList() {
         <Box sx={{margin: '40px auto', width: '45%'}}>
             {predictions && predictions.map((el) => {
                 return (
-                    <>
+                    <Box key={el?.predictionId}>
                         {el.text}
                         <Button onClick={async () => {
                             await approveGHO({args: ['0x1B88a8fef304Ea9413D7224c4Bb878E119A5F329', 1 * WEI]});
                             await betOnPrediction({args: [el?.predictionId, 1 * WEI, true]});
                         }}>Bet</Button>
-                    </>
+                    </Box>
                 )
             })}
         </Box>
