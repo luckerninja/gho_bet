982904b8885edcce2363b61df743bdf8307ca062
Leontiy Yashkov-Kopystynskiy <72278156+luckerninja@users.noreply.github.com>
2024-01-17 18:29:52 +0300

    wagmi betonprediction & approvegho

--- frontend/src/components/PredictionList.tsx
+++ frontend/src/components/PredictionList.tsx
@@ -1,6 +1,7 @@
 
-import { useContractRead } from 'wagmi'
+import { useContractRead, useContractWrite } from 'wagmi'
 import ghoprdAbi from './abi/ghoprd.json'
+import ghoAbi from './abi/gho.json'
 import { Box, Button } from '@mui/material'
 
 export default function PredictionList() {
@@ -9,6 +10,18 @@ export default function PredictionList() {
         address: '0x1B88a8fef304Ea9413D7224c4Bb878E119A5F329',
         abi: ghoprdAbi,
         functionName: 'getPredictions',
+    })
+
+    const { write: approveGHO } = useContractWrite({
+        address: '0xcbE9771eD31e761b744D3cB9eF78A1f32DD99211',
+        abi: ghoAbi,
+        functionName: 'approve',
+    })
+
+    const { write: betOnPrediction } = useContractWrite({
+        address: '0x1B88a8fef304Ea9413D7224c4Bb878E119A5F329',
+        abi: ghoprdAbi,
+        functionName: 'betOnPrediction',
     })
 
     console.log(data)
