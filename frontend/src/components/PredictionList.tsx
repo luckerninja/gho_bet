04601f7021a333ab7b055a9db1f12408f7f43a78
Leontiy Yashkov-Kopystynskiy <72278156+luckerninja@users.noreply.github.com>
2024-01-19 00:10:40 +0300

    prediction list count procents

--- frontend/src/components/PredictionList.tsx
+++ frontend/src/components/PredictionList.tsx
@@ -37,15 +37,17 @@ export default function PredictionList() {
     return(
         <Box sx={{margin: '40px auto', width: '35%', backgroundColor: '#EBE3FA',  borderRadius: '40px', padding: '50px 0'}} >
             {predictions && predictions.map((el) => {
+                const procents = (Number(el.totalAgainst)) /  (Number(el.totalFor) + (Number(el.totalAgainst))) * 100
+
                 return (
                     <>
                         <Box key={el?.predictionId} sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'flex-start', padding: '20px 50px 20px 50px' }} >
                             <Box width='30%' sx={{ display: 'flex', justifyContent: 'center', flexWrap: 'wrap' }}>
                                 <Typography variant='h5'>{el.text}</Typography>
                                 <Box sx={{ display: 'flex', alignItems: 'center', width: '100%', marginTop: '15px'}}>
-                                    <Typography>yes</Typography>
-                                    <LinearProgress variant="determinate"  value={50} sx={{ margin: '0 5px', width: '80%', borderRadius: '10px', height: '17px' }} />
                                     <Typography>no</Typography>
+                                    <LinearProgress variant="determinate" value={procents} sx={{ margin: '0 5px', width: '80%', borderRadius: '10px', height: '17px' }} />
+                                    <Typography>yes</Typography>
                                 </Box>
                             </Box>
                             <Box width='30%' sx={{ display: 'flex', justifyContent: 'center', fontWeight: 'bold' }}>
