561476e96153d0d21937d62e5bd848ad52f810e6
Leontiy Yashkov-Kopystynskiy <72278156+luckerninja@users.noreply.github.com>
2024-01-18 21:59:24 +0300

    prediction list elements

--- frontend/src/components/PredictionList.tsx
+++ frontend/src/components/PredictionList.tsx
@@ -2,7 +2,7 @@
 import { useContractRead, useContractWrite } from 'wagmi'
 import ghoprdAbi from './abi/ghoprd.json'
 import ghoAbi from './abi/gho.json'
-import { Box, Button } from '@mui/material'
+import { Box, Button, Switch, Divider, Input, Typography, LinearProgress } from '@mui/material'
 
 export default function PredictionList() {
 
@@ -26,20 +26,25 @@ export default function PredictionList() {
         functionName: 'betOnPrediction',
     })
 
-    console.log(data)
+    console.log(data, 'predictions')
 
     const predictions = data as any[]
     
     return(
-        <Box sx={{margin: '40px auto', width: '45%'}}>
+        <Box sx={{margin: '40px auto', width: '35%', backgroundColor: '#EBE3FA',  borderRadius: '40px'}}>
             {predictions && predictions.map((el) => {
                 return (
-                    <Box key={el?.predictionId}>
+                    <Box key={el?.predictionId} >
                         {el.text}
                         <Button onClick={async () => {
                             await approveGHO({args: ['0x1B88a8fef304Ea9413D7224c4Bb878E119A5F329', 1 * WEI]});
                             await betOnPrediction({args: [el?.predictionId, 1 * WEI, true]});
                         }}>Bet</Button>
+                        <Typography>{new Date(Number(el.endDate) * 1000).toISOString().slice(0, 10)}</Typography>
+                        <Switch />
+                        <Input type='number' />
+                        <LinearProgress variant="determinate"  value={50} />
+                        <Divider sx={{ height: '2px', backgroundColor: '#A095B5' }} />
                     </Box>
                 )
             })}
